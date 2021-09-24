0c76ce26b95bd45501ed8a21404ba13d29b0da1a
Daniel Adams <msub2official@gmail.com>
2021-09-24 18:53:36 -0400

    Add wallet connection UI

--- js/wallet.js
+++ js/wallet.js
@@ -11,25 +11,76 @@ import { Tzip16Module } from '@taquito/tzip16';
 /* Local Imports */
 import { parseIPFS } from './utils/helpers';
 
+const WALLET_STATE = {
+  DISCONNECTED: 0,
+  CONNECTING: 1,
+  CONNECTED: 2,
+  DISCONNECTING: 3,
+};
+
 WL.registerComponent('wallet', {
     
 }, {
   init: function() {
+    this.ethereumStatus = WALLET_STATE.DISCONNECTED;
+    this.tezosStatus = WALLET_STATE.DISCONNECTED;
+    
+    // Grab HTML elements
     const walletButton = document.getElementById('wallet-button');
     const walletModal = document.getElementById('wallet-modal');
-    const ethereumButton = document.getElementById('ethereum-button');
-    const tezosButton = document.getElementById('tezos-button');
+    const closeModal = document.getElementById('close-modal');
+    this.ethereumButton = document.getElementById('ethereum-button');
+    this.ethereumAddress = document.getElementById('ethereum-address');
+    this.tezosButton = document.getElementById('tezos-button');
+    this.tezosAddress = document.getElementById('tezos-address');
 
+    // Ethereum objects
+    this.web3Modal;
+    this.ethProvider;
+
+    // Tezos objects
+    this.tezosWallet;
+
+    // Setup event listeners
     walletButton.onclick = () => {
       walletModal.style.visibility === 'hidden'
       ? walletModal.style.visibility = 'initial'
       : walletModal.style.visibility = 'hidden';
     }
-    ethereumButton.onclick = () => {
-      this.connectEthereumWallet();
+    closeModal.onclick = () => {
+      walletModal.style.visibility = 'hidden';
     }
-    tezosButton.onclick = () => {
-      this.connectTezosWallet();
+    this.ethereumButton.onclick = () => {
+      switch (this.ethereumStatus) {
+        case WALLET_STATE.DISCONNECTED:
+          this.connectEthereumWallet();
+          break;
+        case WALLET_STATE.DISCONNECTING:
+          console.log("Error: Currently disconnecting existing Ethereum wallet.");
+          break;
+        case WALLET_STATE.CONNECTED:
+          this.disconnectEthereumWallet();
+          break;
+        case WALLET_STATE.CONNECTING:
+          console.log("Error: Currently connecting an Ethereum wallet.");
+          break;
+      }
+    }
+    this.tezosButton.onclick = () => {
+      switch (this.tezosStatus) {
+        case WALLET_STATE.DISCONNECTED:
+          this.connectTezosWallet();
+          break;
+        case WALLET_STATE.DISCONNECTING:
+          console.log("Error: Currently disconnecting existing Tezos wallet.");
+          break;
+        case WALLET_STATE.CONNECTED:
+          this.disconnectTezosWallet();
+          break;
+        case WALLET_STATE.CONNECTING:
+          console.log("Error: Currently connecting a Tezos wallet.");
+          break;
+      }
     }
   },
   start: function() {
@@ -39,6 +90,9 @@ WL.registerComponent('wallet', {
     // Blank
   },
   connectEthereumWallet: async function() {
+    this.ethereumAddress.innerText = "Connecting...";
+    this.ethereumStatus = WALLET_STATE.CONNECTING;
+
     const providerOptions = {
       walletconnect: {
         package: WalletConnectProvider,
@@ -48,50 +102,83 @@ WL.registerComponent('wallet', {
       }
     };
 
-    const web3Modal = new Web3Modal({
+    this.web3Modal = new Web3Modal({
       network: 'mainnet',
       cacheProvider: true,
       providerOptions,
       theme: "dark"
     });
 
-    const provider = await web3Modal.connect();
+    this.ethProvider = await this.web3Modal.connect();
     
-    const ethersProvider = new ethers.providers.Web3Provider(provider);
+    const ethersProvider = new ethers.providers.Web3Provider(this.ethProvider);
     const signer = ethersProvider.getSigner();
 
-    const walletAddress = await ethersProvider.listAccounts();
-    let balance = await ethersProvider.getBalance(walletAddress[0]);
+    const walletAddress = (await ethersProvider.listAccounts())[0];
+    const wallet = await ethersProvider.lookupAddress(walletAddress) || walletAddress;
+    this.ethereumAddress.innerText = wallet;
+    
+    this.ethereumStatus = WALLET_STATE.CONNECTED;
+  },
+  disconnectEthereumWallet: async function() {
+    this.ethereumAddress.innerText = "Disconnecting wallet...";
+    this.ethereumStatus = WALLET_STATE.DISCONNECTING;
+
+    await this.web3Modal.clearCachedProvider();
+    
+    this.ethereumAddress.innerText = 'No wallet connected.';
+    this.ethereumStatus = WALLET_STATE.DISCONNECTED;
+  },
+  getEthereumTokens: async function() {
+    /*let balance = await ethersProvider.getBalance(wallet);
     balance = ethers.utils.formatEther(balance);
 
-    console.log(balance);
+    console.log(balance);*/
   },
   connectTezosWallet: async function() {
+    this.tezosAddress.innerText = "Connecting...";
+    this.tezosStatus = WALLET_STATE.CONNECTING;
+
     const rpcUrl = 'https://api.tez.ie/rpc/mainnet';
     const apiBase = 'https://api.better-call.dev/v1/account';
     let currentAddress;
     const Tezos = new TezosToolkit(rpcUrl);
     Tezos.addExtension(new Tzip16Module());
     const client = new TaquitoTezosDomainsClient({ tezos: Tezos, network: 'mainnet', caching: { enabled: true } });
-    const wallet = new BeaconWallet({
+    this.tezosWallet = new BeaconWallet({
       name: "Aeternus",
       preferredNetwork: NetworkType.MAINNET,
     });
-    const activeAccount = await wallet.client.getActiveAccount();
+    const activeAccount = await this.tezosWallet.client.getActiveAccount();
     if (activeAccount) {
       currentAddress = activeAccount.address;
     }
     else {
-      await wallet.requestPermissions({
+      await this.tezosWallet.requestPermissions({
         network: {
           type: 'mainnet',
         },
       });
 
-      currentAddress = await wallet.getPKH();
+      currentAddress = await this.tezosWallet.getPKH();
     }
-    Tezos.setWalletProvider(wallet);
+    Tezos.setWalletProvider(this.tezosWallet);
     const address = (await client.resolver.resolveAddressToName(currentAddress)) ?? currentAddress;
+    
+    this.tezosAddress.innerText = address;
+    this.tezosStatus = WALLET_STATE.CONNECTED;
+  },
+  disconnectTezosWallet: async function() {
+    this.tezosAddress.innerText = "Disconnecting...";
+    this.tezosStatus = WALLET_STATE.DISCONNECTING;
+
+    await this.tezosWallet.clearActiveAccount();
+
+    this.tezosAddress.innerText = "No wallet connected.";
+    this.tezosStatus = WALLET_STATE.DISCONNECTED;
+  },
+  getTezosTokens: async function() {
+    /* Update this later
     // This needs to eventually check total amount of tokens and grab anything over 50 using offset parameter
     const allTokens = await (await fetch(`${apiBase}/mainnet/${currentAddress}/token_balances?size=50`)).json();
     console.log(allTokens);
@@ -99,5 +186,6 @@ WL.registerComponent('wallet', {
     tokens.forEach(token => token.artifact_uri = parseIPFS(token.artifact_uri));
     console.log("Pulling tokens from: " + address);
     console.log(tokens);
-  },
+    */
+  }
 });
